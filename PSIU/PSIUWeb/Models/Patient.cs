a9d7eea6617c0da26e0a1940caa953823aae7d5f
Leonardo Manoel Oliveira Machado <341484@unoesc.lan>
2023-08-10 22:04:25 -0300

    PSIU

---
+++ PSIU/PSIUWeb/Models/Patient.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PSIUWeb.Models
+{
+    public enum Race {
+        Asiático,
+        Branco,
+        Indio,
+        Negro,
+        Pardo,
+        Outros
+    }
+        
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome Requirido")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Data de Nascimento Requirida")]
+        [Display(BirthDate = "Data de Nascimento")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Peso Requirido")]
+        [Display(Weight = "Peso")]
+        public float Weight { get; set; }
+        // Float, decimal são tipos reais
+
+        [Required(ErrorMessage = "Altura Requirida")]
+        [Display(Height = "Altura")]
+        public decimal Height { get; set; }
+
+        [Required(ErrorMessage = "Raça Requirida")]
+        [Display(Race = "Raça")]
+        public Race Race { get; set; }
+
+        [ForeignKey("User")]
+        public string UserID { get; set; }
+        //Navigation 
+        public appUser User { get; set; }
+
+    }
+}
---
+++ PSIU/PSIUWeb/Models/appUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PSIUWeb.Models
+{
+    public enum Gender { Feminino, Masculino, Outros }
+    public class appUser : IdentityUser
+    {
+        [Required(ErrorMessage = "Nome requirido!")]
+        [Display(Name = "Nome completo.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Data de nascimento requirido!")]
+        [Display(Name = "Data de nascimento")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Genêro requerido")]
+        [Display(Name = "Gênero")]
+        public Gender Gender { get; set; }
+        public DateTime? CreatinDate { get; set;}
+
+    }
+}
